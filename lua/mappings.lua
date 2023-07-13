4674fb914ea19822eead5f927ef90d13b2373654
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-07-13 17:26:49 +0200

    Add some mappings

---
+++ lua/mappings.lua
@@ -0,0 +1,2 @@
+vim.g.mapleader = "<Space>"
+vim.keymap.set("t", "<C-w>N", "<C-\\><C-n>")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,9 @@
 return require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
-  use "mg979/vim-visual-multi"
+  use {
+    "mg979/vim-visual-multi",
+    config = function() 
+      vim.g.VM_leader = vim.g.mapleader
+    end
+  }
 end)
