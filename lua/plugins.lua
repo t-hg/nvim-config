1efbe68d6ec624682c8f179df6b64c5e05419793
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-07-13 22:38:55 +0200

    add nvim-lspconfig

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,15 +1,19 @@
 return require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
-
-  use {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate"
-  }
+  use "nvim-treesitter/nvim-treesitter"
 
   use {
     "mg979/vim-visual-multi",
     config = function() 
       vim.g.VM_leader = vim.g.mapleader
+    end
+  }
+
+  use {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.gopls.setup {}
     end
   }
 end)
