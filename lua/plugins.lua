3350e531a8b0b4c9c5b2b10906eb6fb8f7343b31
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-07-13 21:25:32 +0200

    Add tree-sitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,11 @@
 return require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
+
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate"
+  }
+
   use {
     "mg979/vim-visual-multi",
     config = function() 
