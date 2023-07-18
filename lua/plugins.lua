498033ef65cb77d957f0657141a20bdaa2857cab
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-07-18 07:13:52 +0200

    use telescope, needs neovim 0.9+

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -44,11 +44,26 @@ local function configure_lsp_and_autocompletion(use)
   }
 end
 
+local function configure_telescope(use)
+  use "nvim-lua/plenary.nvim"
+  use {
+    "nvim-telescope/telescope.nvim",
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    end
+  }
+end
+
 return require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
   use "nvim-treesitter/nvim-treesitter"
 
   configure_lsp_and_autocompletion(use)
+  configure_telescope(use)
 
   use {
     "mg979/vim-visual-multi",
