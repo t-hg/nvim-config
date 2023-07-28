b0a3e0195298792d4313291e7cb49b88e052b8d7
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-07-28 08:56:23 +0200

    add theme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -106,6 +106,44 @@ local function configure_file_tree(use)
 	})
 end
 
+local function configure_theme(use)
+	use({
+		"Mofiqul/dracula.nvim",
+		config = function()
+			local theme = require("dracula")
+			theme.setup({
+				colors = {
+					bg = "#282C34",
+					black = "#282C34",
+					bright_blue = "#61AFEF",
+					bright_cyan = "#56B6C2",
+					bright_green = "#98C379",
+					bright_magenta = "#FF92DF",
+					bright_red = "#E06C75",
+					bright_white = "#DCDFE4",
+					bright_yellow = "#E5C07B",
+					comment = "#6272A4",
+					cyan = "#56B6C2",
+					fg = "#F8F8F2",
+					green = "#98C379",
+					gutter_fg = "#4B5263",
+					menu = "#21222C",
+					nontext = "#3B4048",
+					orange = "#FFB86C",
+					pink = "#FF79C6",
+					purple = "#C678DD",
+					red = "#E06C75",
+					selection = "#44475A",
+					visual = "#3E4452",
+					white = "#DCDFE4",
+					yellow = "#E5C07B",
+				},
+			})
+			theme.load()
+		end,
+	})
+end
+
 return require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
 
@@ -115,4 +153,5 @@ return require("packer").startup(function(use)
 	configure_telescope(use)
 	configure_md_image_support(use)
 	configure_file_tree(use)
+	configure_theme(use)
 end)
