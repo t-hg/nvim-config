202a31a0255111eb36c74c9aabc756b3e648d50b
Tobias HORT-GIESS <tobias.hort-giess@compass-security.com>
2023-08-07 17:51:44 +0200

    add command to trim trailing whitespaces

--- lua/commands.lua
+++ lua/commands.lua
@@ -2,3 +2,4 @@ vim = vim
 
 vim.api.nvim_create_user_command("EditConfig", ":cd $HOME/.config/nvim | e init.lua", {})
 vim.api.nvim_create_user_command("ReloadConfig", ":source $HOME/.config/nvim/init.lua", {})
+vim.api.nvim_create_user_command("TrimTrailingWhitespaces", ":%s/\\s\\+$//e", {})
