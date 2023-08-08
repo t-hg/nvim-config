2e8b82a233092e50b8fc5a3ca0e0332e5911a601
Tobias HORT-GIESS <tobias.hort-giess@compass-security.com>
2023-08-08 08:24:24 +0200

    do not install all ts langs by default

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,7 +5,7 @@ local function configure_syntax_highlighting(use)
     "nvim-treesitter/nvim-treesitter",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = "all",
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
         highlight = {
           enable = true,
           additional_vim_regex_highlighting = false,
