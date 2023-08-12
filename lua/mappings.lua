53c532385e171ec2f1372e7ceaa66effae28438f
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-08-12 15:37:59 +0200

    window mappings in terminal mode

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,3 +2,11 @@ vim = vim
 
 vim.g.mapleader = " "
 vim.keymap.set("t", "<C-w>N", "<C-\\><C-n>")
+vim.keymap.set("t", "<C-w>h", "<C-\\><C-n><C-w>h")
+vim.keymap.set("t", "<C-w>j", "<C-\\><C-n><C-w>j")
+vim.keymap.set("t", "<C-w>k", "<C-\\><C-n><C-w>k")
+vim.keymap.set("t", "<C-w>l", "<C-\\><C-n><C-w>l")
+vim.keymap.set("t", "<C-w>H", "<C-\\><C-n><C-w>H")
+vim.keymap.set("t", "<C-w>J", "<C-\\><C-n><C-w>J")
+vim.keymap.set("t", "<C-w>K", "<C-\\><C-n><C-w>K")
+vim.keymap.set("t", "<C-w>L", "<C-\\><C-n><C-w>L")
