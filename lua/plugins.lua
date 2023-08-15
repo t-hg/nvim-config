12034f18fefdf1abc96940e3ae0a1a2685c24fe5
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-08-15 18:50:55 +0200

    bootstrap packer

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -170,6 +170,19 @@ local function configure_text_manipulation_helpers(use)
   })
 end
 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require("packer").startup(function(use)
   use("wbthomason/packer.nvim")
 
@@ -181,4 +194,8 @@ return require("packer").startup(function(use)
   configure_md_image_support(use)
   configure_theme(use)
   configure_text_manipulation_helpers(use)
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
