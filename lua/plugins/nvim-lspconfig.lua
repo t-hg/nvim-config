73a3eb32c1f890666ea5c8d61aadb96d62581557
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-10-05 08:37:31 +0200

    Revert "c lang server"
    This reverts commit f9c887e5e5a089fd5baa899d9f9b59a4d6cb5cea.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -25,7 +25,6 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.ccls.setup({ capabilities = capabilities })
       lspconfig.bashls.setup({ capabilities = capabilities })
       lspconfig.gopls.setup({ capabilities = capabilities })
       lspconfig.lua_ls.setup({ capabilities = capabilities })
