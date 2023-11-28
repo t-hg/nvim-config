8ae143da6059dfdeecb9c69b42db91804ac848b0
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-11-29 00:00:56 +0100

    pyright

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -30,10 +30,11 @@ return {
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
       lspconfig.bashls.setup({ capabilities = capabilities })
+      lspconfig.cssls.setup({ capabilities = capabilities })
       lspconfig.gopls.setup({ capabilities = capabilities })
       lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.pyright.setup({ capabilities = capabilities })
       lspconfig.tsserver.setup({ capabilities = capabilities })
-      lspconfig.cssls.setup({ capabilities = capabilities })
     end,
   },
   "hrsh7th/cmp-vsnip",
