9c01ba86dcb3a2a13e4f9ca4d7728d7ca0ab0e8a
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2024-04-27 15:02:56 +0200

    copy to clipbaord
    Looking for the best copy paste behaviour.
    set clipboard=unnamedplus copies too much.
    Also: I don't like I cannot paste multiple times.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,14 +2,42 @@ vim = vim
 
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "q:", ":q")
+-- Autocorrect q: to :q
+vim.keymap.set("n", "q:", ":q", { noremap = true })
 
-vim.keymap.set("t", "<C-w>N", "<C-\\><C-n>")
-vim.keymap.set("t", "<C-w>h", "<C-\\><C-n><C-w>h")
-vim.keymap.set("t", "<C-w>j", "<C-\\><C-n><C-w>j")
-vim.keymap.set("t", "<C-w>k", "<C-\\><C-n><C-w>k")
-vim.keymap.set("t", "<C-w>l", "<C-\\><C-n><C-w>l")
-vim.keymap.set("t", "<C-w>H", "<C-\\><C-n><C-w>H")
-vim.keymap.set("t", "<C-w>J", "<C-\\><C-n><C-w>J")
-vim.keymap.set("t", "<C-w>K", "<C-\\><C-n><C-w>K")
-vim.keymap.set("t", "<C-w>L", "<C-\\><C-n><C-w>L")
+-- Easier window managment in terminal mode
+vim.keymap.set("t", "<C-w>N", "<C-\\><C-n>", { noremap = true })
+vim.keymap.set("t", "<C-w>h", "<C-\\><C-n><C-w>h", { noremap = true })
+vim.keymap.set("t", "<C-w>j", "<C-\\><C-n><C-w>j", { noremap = true })
+vim.keymap.set("t", "<C-w>k", "<C-\\><C-n><C-w>k", { noremap = true })
+vim.keymap.set("t", "<C-w>l", "<C-\\><C-n><C-w>l", { noremap = true })
+vim.keymap.set("t", "<C-w>H", "<C-\\><C-n><C-w>H", { noremap = true })
+vim.keymap.set("t", "<C-w>J", "<C-\\><C-n><C-w>J", { noremap = true })
+vim.keymap.set("t", "<C-w>K", "<C-\\><C-n><C-w>K", { noremap = true })
+vim.keymap.set("t", "<C-w>L", "<C-\\><C-n><C-w>L", { noremap = true })
+
+-- Yank to clipboard
+vim.keymap.set("n", "Y", "\"+y$", { noremap = true })
+vim.keymap.set("n", "yy", "\"+yy", { noremap = true })
+vim.keymap.set("n", "yi\"", "\"+yi\"", { noremap = true })
+vim.keymap.set("n", "yi'", "\"+yi'", { noremap = true })
+vim.keymap.set("n", "yit", "\"+yit", { noremap = true })
+vim.keymap.set("n", "yi(", "\"+yi(", { noremap = true })
+vim.keymap.set("n", "yi[", "\"+yi[", { noremap = true })
+vim.keymap.set("v", "y", "\"+y", { noremap = true })
+
+-- Cut to clipboard
+vim.keymap.set("n", "D", "\"+d$", { noremap = true })
+vim.keymap.set("n", "dd", "\"+dd", { noremap = true })
+vim.keymap.set("n", "di\"", "\"+di\"", { noremap = true })
+vim.keymap.set("n", "di'", "\"+di'", { noremap = true })
+vim.keymap.set("n", "dit", "\"+dit", { noremap = true })
+vim.keymap.set("n", "di(", "\"+di(", { noremap = true })
+vim.keymap.set("n", "di[", "\"+di[", { noremap = true })
+vim.keymap.set("v", "d", "\"+d", { noremap = true })
+
+-- Paste from clipboard
+vim.keymap.set("n", "p", "\"+p", { noremap = true })
+vim.keymap.set("n", "P", "\"+P", { noremap = true })
+vim.keymap.set("v", "p", "\"+p", { noremap = true })
+vim.keymap.set("v", "P", "\"+P", { noremap = true })
