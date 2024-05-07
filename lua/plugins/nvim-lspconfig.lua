2e533c2554816752e425d583029b728c6f9a4a64
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2024-05-07 22:26:41 +0200

    nimlang server

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -29,6 +29,7 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
+	  lspconfig.nim_langserver.setup({ capabilities = capabilities })
       lspconfig.bashls.setup({ capabilities = capabilities })
       lspconfig.cssls.setup({ capabilities = capabilities })
       lspconfig.gleam.setup({ capabilities = capabilities })
