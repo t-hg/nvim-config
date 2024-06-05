f78431bf2a0e2120c396b83b56275329aa011909
Tobias Hort-Giess <tobias.hort-giess@compass-security.com>
2024-06-05 16:13:09 +0200

    redact, ellipsis

--- lua/commands.lua
+++ lua/commands.lua
@@ -6,3 +6,5 @@ vim.api.nvim_create_user_command("EditConfig", ":cd $HOME/.config/nvim | e init.
 vim.api.nvim_create_user_command("ReloadConfig", ":source $HOME/.config/nvim/init.lua", {})
 vim.api.nvim_create_user_command("TrimLeadingWhitespaces", "<line1>,<line2>s/^\\s*// | noh", {range = "%"})
 vim.api.nvim_create_user_command("TrimTrailingWhitespaces", "<line1>,<line2>s/\\s*$// | noh", {range = "%"})
+vim.api.nvim_create_user_command("Redact", "norm gvr█", {range = true})
+vim.api.nvim_create_user_command("Ellipsis", "norm gvc[...]", {range = true})
